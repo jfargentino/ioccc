110904613cedb2c8d1d7eb4f8d127e3abdc75f21
JF Argentino <jf.argentino@free.fr>
2018-01-10 00:10:10 +0100

    colors

--- self/make_all.sh
+++ self/make_all.sh
@@ -11,9 +11,15 @@ _do_one () {
         ./self-"$k" | sed '/^$/d' > self-"$k".d
         if [[ -f self-"$k".d ]]; then
             diff -B --strip-trailing-cr self-"$k".c self-"$k".d > self-"$k".e
-            echo -e "\e[42m###\e[0m\e[32mself-$k\e[0m"
-            cat self-"$k".e
-            echo -e "\e[42m###\e[0m"
+            sz=$(wc -c < self-"$k".e)
+            if [[ $sz -eq 0 ]]; then
+                echo -e "\e[42m###\e[0m\e[32mself-$k\e[0m"
+                rm self-"$k".e
+            else
+                echo -e "\e[43m???\e[0m\e[33mself-$k\e[0m"
+                cat self-"$k".e
+                echo -e "\e[43m???\e[0m"
+            fi
         else
             echo -e "\e[41m!!! ERROR executing self-$k !!!\e[0m"
         fi
