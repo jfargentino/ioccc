609fba19003d96d7968cc60ae83b18a1661649e1
JF Argentino <jf.argentino@free.fr>
2018-01-09 22:48:08 +0100

    some scripts

--- self/split.sh
+++ self/split.sh
@@ -3,5 +3,5 @@
 sed -i 's/^Author/\/\/Author/' self_c.txt
 sed -i 's/^Notes/\/\/Notes/' self_c.txt
 N=63
-csplit -k -b '-%02d.c' -fself -z self_c.txt '/Author/' {$N}
+csplit -k -b '-%02d.c' -fself -z self_c.txt '/Author/' "{$N}"
 
