5623034cc65c3c055b3ea2ddce7dab6e90d1189b
jfargentino <jf.argentino@free.fr>
2018-03-24 02:31:59 +0100

    check

--- xor/check.sh
+++ xor/check.sh
@@ -13,5 +13,6 @@ orig/uuid4/example/example 2> .SEED 1> .UUID_REF
 SEED0=$(awk '{print $1}' .SEED)
 SEED1=$(awk '{print $2}' .SEED)
 ./uuid4 -s "$SEED0" "$SEED1" > .UUID
+cat .UUID .UUID_REF
 diff .UUID .UUID_REF
 
